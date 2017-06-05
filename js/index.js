80266af85ec437af5471b9eb7fa2163901365c21
wangbiaozy <56565283@qq.com>
2017-06-05 14:45:21 +0800

    1

---
+++ js/index.js
@@ -0,0 +1,52 @@
+(function(global){
+    function remChange(){
+        document.documentElement.style.fontSize=20*document.documentElement.clientWidth/720+'px';
+    }
+    remChange();
+    global.addEventListener('resize',remChange,false);
+})(window);
+Zepto(function () {
+    $('#jc1').on('touchend',function () {
+        window.location.href = 'http://wapzt.189.cn/pages/variousPackage/various_detail.html?salesId=0000000050F05EC57F5E7F92E053AA1410AC8D10';
+    });
+    $('#jc2').on('touchend',function () {
+        window.location.href = 'http://wapzt.189.cn/pages/variousPackage/various_detail.html?salesId=0000000036753048358C3F0BE053AE1410AC1C8C';
+    });
+    $('#jc3').on('touchend',function () {
+        window.location.href = 'http://wapzt.189.cn/pages/variousPackage/various_detail.html?salesId=0000000050F0C6F0EC531156E053AC1410ACF990';
+    });
+    $('#jc4').on('touchend',function () {
+        window.location.href = 'http://wapzt.189.cn/pages/variousPackage/various_detail.html?salesId=00000000367709FB848B7BC1E053AC1410ACFE8C';
+    });
+    $('#jc5').on('touchend',function () {
+        window.location.href = 'http://wapzt.189.cn/pages/variousPackage/various_detail.html?salesId=00000000367709FB851A7BC1E053AC1410ACFE8C';
+    });
+    $('#jc6').on('touchend',function () {
+        window.location.href = 'http://wapzt.189.cn/pages/variousPackage/various_detail.html?salesId=0000000050F05EC57F857F92E053AA1410AC8D10';
+    });
+
+    $('#jb1').on('touchend',function () {
+        window.location.href = 'http://wapzt.189.cn/pages/variousPackage/various_detail.html?salesId=000000003D9D0694AD712BAEE053AA1410ACE818';
+    });
+    $('#jb2').on('touchend',function () {
+        window.location.href = 'http://wapzt.189.cn/pages/variousPackage/various_detail.html?salesId=000000003D9CFD52D19541D0E053AC1410AC0130';
+    });
+    $('#jb3').on('touchend',function () {
+        window.location.href = 'http://wapzt.189.cn/pages/variousPackage/various_detail.html?salesId=000000003D9D06955EF82BB0E053AA1410ACB971';
+    });
+    $('#jb4').on('touchend',function () {
+        window.location.href = 'http://wapzt.189.cn/pages/variousPackage/various_detail.html?salesId=000000003D9D172337AB7FE9E053AA1410ACDBC0';
+    });
+    $('#bn1').on('touchend',function () {
+        window.location.href = 'http://wapzt.189.cn/pages/variousPackage/various_detail.html?salesId=000000003D9D0694ADA32BAEE053AA1410ACE818';
+    });
+    $('#bn2').on('touchend',function () {
+        window.location.href = 'http://wapzt.189.cn/pages/variousPackage/various_detail.html?salesId=000000003D9BFF512405441BE053AC1410AC1117';
+    });
+    $('#bn3').on('touchend',function () {
+        window.location.href = 'http://wapzt.189.cn/pages/variousPackage/various_detail.html?salesId=000000003D9BF4CC02311656E053AC1410AC10ED';
+    });
+    $('#nb1').on('touchend',function () {
+        window.location.href = 'http://wapzt.189.cn/pages/variousPackage/various_detail.html?salesId=000000003D9CB06D3DEE3164E053AC1410ACB0D5';
+    });
+});
